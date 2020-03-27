aa8bf30844cb09d900adaea41aafb4d2eef90149
Master Adit <aditluhadia@gmail.com>
2020-03-27 23:30:28 +0530

    Removed unnecessary imports in main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:note_keeper_app/screens/note_list.dart';
-import 'package:note_keeper_app/screens/note_detail.dart';
 
 void main() {
   runApp(MyApp());
